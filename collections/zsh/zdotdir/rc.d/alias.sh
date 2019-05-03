cf04d0a86ae0f316c3457dc94376c58cc127eb80
Evan Rusmisel <evanrusmisel@gmail.com>
2019-05-03 18:19:04 -0500

    Add music stuff

--- collections/zsh/zdotdir/rc.d/alias.sh
+++ collections/zsh/zdotdir/rc.d/alias.sh
@@ -1,6 +1,8 @@
 alias v=vim
 alias sudo='sudo -E'
-alias cb="xclip -i -selection clipboard"	# Pipe text to clipboard
-alias cdc="cd \"\$(xclip -o -selection clipboard)\""	# cd to directory in clipboard
-alias ls="ls --color=auto --group -FhN"
+alias cb='xclip -i -selection clipboard'	# Pipe text to clipboard
+alias cdc='cd "\$(xclip -o -selection clipboard)"'	# cd to directory in clipboard
+alias ls='ls --color=auto --group -FhN'
 alias grep='grep --color=auto'
+alias download-album='youtube-dl --config-location ~/.config/youtube-dl/album'
+alias download-track='youtube-dl --config-location ~/.config/youtube-dl/track'
