fd679d88dacdfc08486a1d0ee2ee17a905b80b3d
Evan Rusmisel <evanrusmisel@gmail.com>
2019-05-03 18:48:56 -0500

    Fix cdc alias in zsh collection

--- collections/zsh/zdotdir/rc.d/alias.sh
+++ collections/zsh/zdotdir/rc.d/alias.sh
@@ -1,7 +1,7 @@
 alias v=vim
 alias sudo='sudo -E'
 alias cb='xclip -i -selection clipboard'	# Pipe text to clipboard
-alias cdc='cd "\$(xclip -o -selection clipboard)"'	# cd to directory in clipboard
+alias cdc='cd "$(xclip -o -selection clipboard)"'	# cd to directory in clipboard
 alias ls='ls --color=auto --group -FhN'
 alias grep='grep --color=auto'
 alias download-album='youtube-dl --config-location ~/.config/youtube-dl/album'
