259a5a87b2698f4e96d8e67818cddef2db67e4d8
Evan Rusmisel <contact@evanr.us>
2019-05-07 10:58:19 -0500

    Modify zsh settings

--- collections/zsh/zdotdir/env.d/vars.sh
+++ collections/zsh/zdotdir/env.d/vars.sh
@@ -1,8 +1,7 @@
 export CODE_DIR="$HOME/Code"
 export PORT=8080
-export PS1="%{[38;5;1m%}%1/%{[0m%} ➜  "
 export GOROOT="/usr/local/go"
-export GOPATH="$HOME/code/go"
+export GOPATH="$CODE_DIR/go"
 
 export EDITOR="vim"
 export VISUAL="vim"
@@ -11,7 +10,6 @@ export PATH="\
 $PATH:\
 $HOME/.deno/bin:\
 $HOME/.local/bin:\
-$HOME/code/bin:\
 $GOROOT/bin:\
 $GOPATH/bin:\
 $HOME/.cargo/bin"
--- collections/zsh/zdotdir/rc.d/settings.zsh
+++ collections/zsh/zdotdir/rc.d/settings.zsh
@@ -3,6 +3,10 @@ HISTSIZE=50
 SAVEHIST=100
 bindkey -v
 
+export PS1="%{%F{1}%}%1/%{%f%} ➜  "
+
+[ -n "$(tput colors)" ] && [ -r "$HOME/.cache/wal/sequences" ] && cat $HOME/.cache/wal/sequences
+
 zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
 zstyle ':completion:*' max-errors 3
 zstyle :compinstall filename '/home/enva2712/.config/zsh/.zshrc'
