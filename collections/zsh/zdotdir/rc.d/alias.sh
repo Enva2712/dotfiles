fbd824da2170292611a65c568050b2633e68b652
Evan Rusmisel <evanrusmisel@gmail.com>
2019-07-25 13:16:04 -0500

    zsh: add alias for info to use --vi-keys

--- collections/zsh/zdotdir/rc.d/alias.sh
+++ collections/zsh/zdotdir/rc.d/alias.sh
@@ -4,5 +4,6 @@ alias cb='xclip -i -selection clipboard'	# Pipe text to clipboard
 alias cdc='cd "$(xclip -o -selection clipboard)"'	# cd to directory in clipboard
 alias ls='ls --color=auto --group -FhN'
 alias grep='grep --color=auto'
+alias info='info --vi-keys'
 alias download-album='youtube-dl --config-location ~/.config/youtube-dl/album'
 alias download-track='youtube-dl --config-location ~/.config/youtube-dl/track'
