e5fbcaf4b708e23f49e8037c97a5338b9cfa7f22
Evan Rusmisel <contact@evanr.us>
2019-07-30 17:58:23 -0500

    Add bc alias to source config

--- collections/zsh/zdotdir/rc.d/alias.sh
+++ collections/zsh/zdotdir/rc.d/alias.sh
@@ -4,5 +4,7 @@ alias cb='xclip -i -selection clipboard'	# Pipe text to clipboard
 alias cdc='cd "$(xclip -o -selection clipboard)"'	# cd to directory in clipboard
 alias ls='ls --color=auto --group -FhN'
 alias grep='grep --color=auto'
+alias bc='bc -ql $CONFIG_DIR/bc'
+
 alias youtube-dl-album='youtube-dl --config-location $CONFIG_DIR/youtube-dl/album'
 alias youtube-dl-track='youtube-dl --config-location $CONFIG_DIR/youtube-dl/track'
