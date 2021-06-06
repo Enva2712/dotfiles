4922819c4225e97c9f28f7e38272f61c8c6e0ec8
Evan Rusmisel <rusmisele@dnb.com>
2021-06-06 14:49:11 -0500

    Mac compat

--- collections/zsh/zdotdir/rc.d/alias.sh
+++ collections/zsh/zdotdir/rc.d/alias.sh
@@ -2,10 +2,18 @@ alias v=vim
 alias sudo='sudo -E'
 alias cb='xclip -i -selection clipboard'	# Pipe text to clipboard
 alias cdc='cd "$(xclip -o -selection clipboard)"'	# cd to directory in clipboard
-alias ls='ls --color=auto --group -FhN'
 alias grep='grep --color=auto'
 alias info='info --vi-keys'
 alias bc='bc -ql $CONFIG_DIR/bc'
+
+case "$(uname -s)" in
+	(Linux*)
+		alias ls='ls --color=auto --group -FhN'
+	;;
+	(Darwin*)
+		alias ls='ls -FhG'
+	;;
+esac
 
 alias youtube-dl-album='youtube-dl --config-location $CONFIG_DIR/youtube-dl/album'
 alias youtube-dl-track='youtube-dl --config-location $CONFIG_DIR/youtube-dl/track'
