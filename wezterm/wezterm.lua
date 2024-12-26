797b95aec0a67ec5aa20d94cfb89c5af49f18b1e
Evan Rusmisel <evanrusmisel@gmail.com>
2024-12-26 03:57:34 -0600

    blurs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   hide_tab_bar_if_only_one_tab = true,
-  window_background_opacity = .8,
+  window_background_opacity = .5,
   font = wezterm.font_with_fallback { 'Iosevka Term', 'Font Awesome 6 Free Regular', 'Font Awesome 6 Free Solid' },
   font_size = 14,
 }
